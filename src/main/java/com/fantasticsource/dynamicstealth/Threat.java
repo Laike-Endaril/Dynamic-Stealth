e167275db472bc46f4ac363aa0f1d650fe677f20
Kyle Koder <silvertallon@gmail.com>
2018-12-23 12:01:57 -0800

    Fixed issue where Threat class was using EntityLiving instead of EntityLivingBase for threat targets

--- src/main/java/com/fantasticsource/dynamicstealth/Threat.java
+++ src/main/java/com/fantasticsource/dynamicstealth/Threat.java
@@ -4,11 +4,12 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 
 public class Threat
 {
     //Searcher EntityLiving, current target EntityLiving, current threat
-    private static Map<EntityLiving, Pair<EntityLiving, Integer>> threatMap = new LinkedHashMap<>(200);
+    private static Map<EntityLiving, Pair<EntityLivingBase, Integer>> threatMap = new LinkedHashMap<>(200);
 
 
     public static void remove(EntityLiving searcher)
@@ -17,21 +18,21 @@ public class Threat
     }
 
 
-    public static Pair<EntityLiving, Integer> get(EntityLiving searcher)
+    public static Pair<EntityLivingBase, Integer> get(EntityLiving searcher)
     {
         return threatMap.get(searcher);
     }
 
-    public static EntityLiving getTarget(EntityLiving searcher)
+    public static EntityLivingBase getTarget(EntityLiving searcher)
     {
-        Pair<EntityLiving, Integer> entry = threatMap.get(searcher);
+        Pair<EntityLivingBase, Integer> entry = threatMap.get(searcher);
         if (entry != null) return entry.getKey();
         return null;
     }
 
     public static Integer getThreat(EntityLiving searcher)
     {
-        Pair<EntityLiving, Integer> entry = threatMap.get(searcher);
+        Pair<EntityLivingBase, Integer> entry = threatMap.get(searcher);
         if (entry != null) return entry.getValue();
         return 0;
     }
@@ -39,21 +40,21 @@ public class Threat
 
     public static void set(EntityLiving searcher, EntityLiving target, int threat)
     {
-        Pair<EntityLiving, Integer> entry = threatMap.get(searcher);
+        Pair<EntityLivingBase, Integer> entry = threatMap.get(searcher);
         if (entry != null) entry.set(target, threat);
         else threatMap.put(searcher, new Pair<>(target, threat));
     }
 
     public static void setTarget(EntityLiving searcher, EntityLiving target)
     {
-        Pair<EntityLiving, Integer> entry = threatMap.get(searcher);
+        Pair<EntityLivingBase, Integer> entry = threatMap.get(searcher);
         if (entry != null) entry.setKey(target);
         else threatMap.put(searcher, new Pair<>(target, 0));
     }
 
     public static void setThreat(EntityLiving searcher, int threat)
     {
-        Pair<EntityLiving, Integer> entry = threatMap.get(searcher);
+        Pair<EntityLivingBase, Integer> entry = threatMap.get(searcher);
         if (entry != null) entry.setValue(threat);
         else threatMap.put(searcher, new Pair<>(null, threat));
     }
