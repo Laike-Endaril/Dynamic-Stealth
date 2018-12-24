67d14c614c4178686d832d2576d4de745d8007af
Kyle Koder <silvertallon@gmail.com>
2018-12-24 02:06:15 -0800

    1.12.2.007
    Did some basic testing and so far the system seems to be working as intended; ready for outside testing!
    Hopefully!

--- src/main/java/com/fantasticsource/dynamicstealth/DynamicStealth.java
+++ src/main/java/com/fantasticsource/dynamicstealth/DynamicStealth.java
@@ -45,7 +45,7 @@ public class DynamicStealth
 {
     public static final String MODID = "dynamicstealth";
     public static final String NAME = "Dynamic Stealth";
-    public static final String VERSION = "1.12.2.006";
+    public static final String VERSION = "1.12.2.007";
 
     private static Logger logger;
 
