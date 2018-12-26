ecff084f21a39c6bb68fb7b9997a57a247aef58b
Kyle Koder <silvertallon@gmail.com>
2018-12-26 01:29:23 -0800

    Added Minecraft-specific tool class with generally useful functions.  Or at least ONE generally useful function.

--- src/main/java/com/fantasticsource/mctools/MCTools.java
+++ src/main/java/com/fantasticsource/mctools/MCTools.java
@@ -1,5 +1,12 @@
 package com.fantasticsource.mctools;
 
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+
 public class MCTools
 {
+    public static boolean isOP(EntityPlayerMP player)
+    {
+        return FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().canSendCommands(player.getGameProfile());
+    }
 }
