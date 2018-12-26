81e23dc2c7f7eb83ee532d2c90b74f4a21f0f47a
Kyle Koder <silvertallon@gmail.com>
2018-12-26 01:37:05 -0800

    1.12.2.008

--- src/main/java/com/fantasticsource/dynamicstealth/common/DynamicStealth.java
+++ src/main/java/com/fantasticsource/dynamicstealth/common/DynamicStealth.java
@@ -55,7 +55,7 @@ public class DynamicStealth
 {
     public static final String MODID = "dynamicstealth";
     public static final String NAME = "Dynamic Stealth";
-    public static final String VERSION = "1.12.2.007";
+    public static final String VERSION = "1.12.2.008";
 
     private static Logger logger;
 
