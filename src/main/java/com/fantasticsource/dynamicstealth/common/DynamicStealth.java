f0cd2f19c42dda0fb76870b3d1bc092e3da15518
Kyle Koder <silvertallon@gmail.com>
2018-12-27 02:04:04 -0800

    1.12.2.010a ... for allowing the Faerun team to test it on their build

--- src/main/java/com/fantasticsource/dynamicstealth/common/DynamicStealth.java
+++ src/main/java/com/fantasticsource/dynamicstealth/common/DynamicStealth.java
@@ -58,7 +58,7 @@ public class DynamicStealth
 {
     public static final String MODID = "dynamicstealth";
     public static final String NAME = "Dynamic Stealth";
-    public static final String VERSION = "1.12.2.010";
+    public static final String VERSION = "1.12.2.010a";
 
     private static Logger logger;
 
