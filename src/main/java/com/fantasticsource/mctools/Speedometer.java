c060e52da6a48fcce294a3a04130dfc042546965
Kyle Koder <silvertallon@gmail.com>
2018-12-28 05:56:36 -0800

    Speedometer now accounts for teleports / dimension changes (should not return an extra high result from either one now)

--- src/main/java/com/fantasticsource/mctools/Speedometer.java
+++ src/main/java/com/fantasticsource/mctools/Speedometer.java
@@ -1,35 +1,75 @@
 package com.fantasticsource.mctools;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.math.Vec3d;
+import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
+import net.minecraftforge.event.entity.living.EnderTeleportEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Speedometer
 {
-    //This thing has a lot of caveats eg...
-    //...player motion is smoothed over several updates, but other entities are not
-    //...when teleporting long distances, players return an inaccurate, extremely high value until it's out of the smoothing system (currently 5 updates; about 1/4 second)
+    //Player motion is smoothed over several updates to help account for inconsistent packet reception rates, but other entities are not
 
-    public static Map<EntityPlayerMP, Entry> playerMap = new HashMap<>(1000);
+    public static Map<EntityPlayerMP, Entry> playerMap = new LinkedHashMap<>(1000);
+    public static Map<EntityLiving, Integer> recentTeleports = new LinkedHashMap<>(10);
 
     @SubscribeEvent
-    public static void playerUpdate(TickEvent.PlayerTickEvent event)
+    public static void playerUpdateEvent(TickEvent.PlayerTickEvent event)
     {
         EntityPlayer player = event.player;
-        if (event.phase == TickEvent.Phase.END && player instanceof EntityPlayerMP) update((EntityPlayerMP) player);
+        if (event.phase == TickEvent.Phase.END && player instanceof EntityPlayerMP) updatePlayer((EntityPlayerMP) player);
+    }
+
+    @SubscribeEvent
+    public static void teleport(EnderTeleportEvent event)
+    {
+        EntityLivingBase livingBase = event.getEntityLiving();
+        if (livingBase instanceof EntityPlayerMP) remove((EntityPlayerMP) livingBase);
+        else if (livingBase instanceof EntityLiving) recentTeleports.put((EntityLiving) livingBase, 3);
+    }
+
+    @SubscribeEvent
+    public static void dimChange(EntityTravelToDimensionEvent event)
+    {
+        Entity entity = event.getEntity();
+        if (entity instanceof EntityPlayerMP) remove((EntityPlayerMP) entity);
+        else if (entity instanceof EntityLiving) recentTeleports.put((EntityLiving) entity, 3);
     }
 
     @SubscribeEvent
     public static void playerLogout(PlayerEvent.PlayerLoggedOutEvent event)
     {
-        if (event.player instanceof EntityPlayerMP) playerMap.remove(event.player);
+        if (event.player instanceof EntityPlayerMP) remove((EntityPlayerMP) event.player);
+    }
+
+    @SubscribeEvent
+    public static void serverTick(TickEvent.ServerTickEvent event)
+    {
+        recentTeleports.entrySet().removeIf(Speedometer::checkRemoveTeleporting);
+    }
+
+    private static boolean checkRemoveTeleporting(Map.Entry<EntityLiving, Integer> entry)
+    {
+        int time = entry.getValue();
+        if (--time >= 0) return true;
+
+        entry.setValue(time);
+        return false;
+    }
+
+
+    private static void remove(EntityPlayerMP player)
+    {
+        playerMap.remove(player);
     }
 
     public static double getSpeed(Entity entity)
@@ -40,10 +80,12 @@ public class Speedometer
             return entry == null ? 0 : entry.calculatedSpeed;
         }
 
+        if (recentTeleports.containsKey(entity)) return 0;
+
         return new Vec3d(entity.prevPosX, entity.prevPosY, entity.prevPosZ).distanceTo(entity.getPositionVector()) * 20;
     }
 
-    public static double update(EntityPlayerMP player)
+    public static double updatePlayer(EntityPlayerMP player)
     {
         Vec3d newVec = player.getPositionVector();
 
