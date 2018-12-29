9b5dc0e667d2896e37a0cd0f195f6cb419f39341
Kyle Koder <silvertallon@gmail.com>
2018-12-28 19:53:36 -0800

    Added printTasks(EntityLiving) to MCTools

--- src/main/java/com/fantasticsource/mctools/MCTools.java
+++ src/main/java/com/fantasticsource/mctools/MCTools.java
@@ -1,7 +1,9 @@
 package com.fantasticsource.mctools;
 
 import com.fantasticsource.dynamicstealth.server.ai.AIHurtByTargetEdit;
+import com.fantasticsource.tools.datastructures.ExplicitPriorityQueue;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAITasks;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -23,5 +25,46 @@ public class MCTools
             || task.action instanceof AIHurtByTargetEdit) return false;
         }
         return true;
+    }
+
+    public static void printTasks(EntityLiving living)
+    {
+        ExplicitPriorityQueue<EntityAIBase> queue = new ExplicitPriorityQueue<>();
+        EntityAIBase ai;
+        String str;
+        double priority;
+
+        System.out.println("===================================");
+        System.out.println(living.getName());
+        System.out.println("===================================");
+        for (EntityAITasks.EntityAITaskEntry task : living.targetTasks.taskEntries)
+        {
+            queue.add(task.action, task.priority);
+        }
+        while(queue.size() > 0)
+        {
+            priority = queue.peekPriority();
+            ai = queue.poll();
+            str = ai.getClass().getSimpleName();
+            if (str.equals("")) str = ai.getClass().getName();
+            if (str.equals("")) str = ai.getClass().getPackage().getName() + ".???????";
+            System.out.println(priority + "\t" + str);
+        }
+        System.out.println("===================================");
+        for (EntityAITasks.EntityAITaskEntry task : living.tasks.taskEntries)
+        {
+            queue.add(task.action, task.priority);
+        }
+        while(queue.size() > 0)
+        {
+            priority = queue.peekPriority();
+            ai = queue.poll();
+            str = ai.getClass().getSimpleName();
+            if (str.equals("")) str = ai.getClass().getName();
+            if (str.equals("")) str = ai.getClass().getPackage().getName() + ".???????";
+            System.out.println(priority + "\t" + str);
+        }
+        System.out.println("===================================");
+        System.out.println();
     }
 }
