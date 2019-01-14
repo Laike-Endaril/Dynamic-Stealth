a2114e36a81f82433103b8d7f8a6f10735c82708
Kyle Koder <silvertallon@gmail.com>
2019-01-14 09:51:31 -0800

    Revert "1.12.2.028"
    This reverts commit 429811937008c58bafb494f8b398ab4f256ba9e9.

--- src/main/java/com/fantasticsource/dynamicstealth/common/DynamicStealth.java
+++ src/main/java/com/fantasticsource/dynamicstealth/common/DynamicStealth.java
@@ -65,7 +65,7 @@ public class DynamicStealth
 {
     public static final String MODID = "dynamicstealth";
     public static final String NAME = "Dynamic Stealth";
-    public static final String VERSION = "1.12.2.028";
+    public static final String VERSION = "1.12.2.027b";
 
     public static final TrigLookupTable TRIG_TABLE = new TrigLookupTable(1024);
 
