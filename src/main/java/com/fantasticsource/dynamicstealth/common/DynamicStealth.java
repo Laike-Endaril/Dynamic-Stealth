6a695abbffdf604ed241b3f4d7553b0cdb231ca8
Kyle Koder <silvertallon@gmail.com>
2019-01-21 13:20:30 -0800

    1.12.2.034
    This was added due to bad 033 build caused by corrupted intelliJ cache on my end, ie. no difference as far as the repo is concerned since the cache is not part of the repo

--- src/main/java/com/fantasticsource/dynamicstealth/common/DynamicStealth.java
+++ src/main/java/com/fantasticsource/dynamicstealth/common/DynamicStealth.java
@@ -68,7 +68,7 @@ public class DynamicStealth
 {
     public static final String MODID = "dynamicstealth";
     public static final String NAME = "Dynamic Stealth";
-    public static final String VERSION = "1.12.2.033";
+    public static final String VERSION = "1.12.2.034";
 
     public static final TrigLookupTable TRIG_TABLE = new TrigLookupTable(1024);
 
