4bfdd617c8a4f408ba5931402345690be3a63d45
Kyle Koder <silvertallon@gmail.com>
2019-02-05 17:17:06 -0800

    Made sure AIDynamicStealth is only added to CNPCs on server-side

--- src/main/java/com/fantasticsource/dynamicstealth/compat/CompatCNPC.java
+++ src/main/java/com/fantasticsource/dynamicstealth/compat/CompatCNPC.java
@@ -1,6 +1,7 @@
 package com.fantasticsource.dynamicstealth.compat;
 
 import com.fantasticsource.dynamicstealth.server.ai.AIDynamicStealth;
+import com.fantasticsource.mctools.MCTools;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraftforge.event.entity.living.LivingEvent;
@@ -14,7 +15,7 @@ public class CompatCNPC
     public static void livingUpdate(LivingEvent.LivingUpdateEvent event) throws Exception
     {
         Entity entity = event.getEntity();
-        if (entity instanceof EntityLiving)
+        if (entity instanceof EntityLiving && !MCTools.isClient(entity.world))
         {
             EntityLiving living = (EntityLiving) entity;
             if (AIDynamicStealth.getStealthAI(living) == null) entityJoinWorldInit(living);
