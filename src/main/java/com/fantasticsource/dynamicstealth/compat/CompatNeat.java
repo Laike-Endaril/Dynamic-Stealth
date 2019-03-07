a8f3f5b8703853f07d2ba6497325d7fdd3732ef0
Kyle Koder <silvertallon@gmail.com>
2019-03-07 03:19:46 -0800

    Removed unused vars

--- src/main/java/com/fantasticsource/dynamicstealth/compat/CompatNeat.java
+++ src/main/java/com/fantasticsource/dynamicstealth/compat/CompatNeat.java
@@ -9,6 +9,4 @@ public class CompatNeat
 {
     public static Configuration config = new Configuration(new File(Loader.instance().getConfigDir(), "neat.cfg"));
     public static double heightAboveMob = config.get("general", "Height Above Mob", 0.6).getDouble();
-    public static int plateSize = config.get("general", "Plate Size", 25).getInt();
-    public static int bossPlateSize = config.get("general", "Plate Size (Boss)", 50).getInt();
 }
