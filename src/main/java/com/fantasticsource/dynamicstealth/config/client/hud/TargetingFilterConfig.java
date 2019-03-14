d6f832fd894a9d47ccacde39bd17c9ca545845a8
Kyle Koder <silvertallon@gmail.com>
2019-03-14 15:21:04 -0700

    Fixed some config tooltips

--- src/main/java/com/fantasticsource/dynamicstealth/config/client/hud/TargetingFilterConfig.java
+++ src/main/java/com/fantasticsource/dynamicstealth/config/client/hud/TargetingFilterConfig.java
@@ -5,30 +5,30 @@ import net.minecraftforge.common.config.Config;
 public class TargetingFilterConfig
 {
     @Config.Name("Passive")
-    @Config.Comment({"If true, the detail HUD shows when focused on a passive entity"})
+    @Config.Comment({"If true, the targeting HUD shows when focused on a passive entity"})
     public boolean showPassive = false;
 
     @Config.Name("Bypass")
-    @Config.Comment({"If true, the detail HUD shows when focused on an entity that bypasses the threat system"})
+    @Config.Comment({"If true, the targeting HUD shows when focused on an entity that bypasses the threat system"})
     public boolean showBypass = true;
 
     @Config.Name("Idle")
-    @Config.Comment({"If true, the detail HUD shows when focused on an idle entity"})
+    @Config.Comment({"If true, the targeting HUD shows when focused on an idle entity"})
     public boolean showIdle = true;
 
     @Config.Name("Attacking Other")
-    @Config.Comment({"If true, the detail HUD shows when focused on an entity that is attacking something besides you"})
+    @Config.Comment({"If true, the targeting HUD shows when focused on an entity that is attacking something besides you"})
     public boolean showAttackingOther = true;
 
     @Config.Name("Alert")
-    @Config.Comment({"If true, the detail HUD shows when focused on an entity that is actively searching for a target"})
+    @Config.Comment({"If true, the targeting HUD shows when focused on an entity that is actively searching for a target"})
     public boolean showAlert = true;
 
     @Config.Name("Attacking You")
-    @Config.Comment({"If true, the detail HUD shows when focused on an entity that is attacking YOU"})
+    @Config.Comment({"If true, the targeting HUD shows when focused on an entity that is attacking YOU"})
     public boolean showAttackingYou = true;
 
     @Config.Name("Flee")
-    @Config.Comment({"If true, the detail HUD shows when focused on an entity that is fleeing from combat"})
+    @Config.Comment({"If true, the targeting HUD shows when focused on an entity that is fleeing from combat"})
     public boolean showFleeing = true;
 }
