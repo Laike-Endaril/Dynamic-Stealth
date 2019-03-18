a653303dabcaa8db973ec1e23208a751cc8cc553
Kyle Koder <silvertallon@gmail.com>
2019-03-18 14:42:40 -0700

    Doesn't require next lib version quite yet; fixing requirement

--- src/main/java/com/fantasticsource/dynamicstealth/common/DynamicStealth.java
+++ src/main/java/com/fantasticsource/dynamicstealth/common/DynamicStealth.java
@@ -89,7 +89,7 @@ import java.util.Set;
 
 import static com.fantasticsource.dynamicstealth.config.DynamicStealthConfig.serverSettings;
 
-@Mod(modid = DynamicStealth.MODID, name = DynamicStealth.NAME, version = DynamicStealth.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.007a,)")
+@Mod(modid = DynamicStealth.MODID, name = DynamicStealth.NAME, version = DynamicStealth.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.007,)")
 public class DynamicStealth
 {
     public static final String MODID = "dynamicstealth";
