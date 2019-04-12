d568d9825f98c6cdbadafa5bb02741a2c0868f09
Kyle Koder <silvertallon@gmail.com>
2019-04-11 22:22:57 -0700

    Revamped tracking system, may or may not use it for an upcoming feature

--- src/main/java/com/fantasticsource/dynamicstealth/server/senses/sight/Tracking.java
+++ src/main/java/com/fantasticsource/dynamicstealth/server/senses/sight/Tracking.java
@@ -1,49 +1,54 @@
 package com.fantasticsource.dynamicstealth.server.senses.sight;
 
-import com.fantasticsource.tools.Tools;
+import com.fantasticsource.tools.datastructures.Pair;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class Tracking
 {
-    private static LinkedHashMap<EntityLivingBase, LinkedHashMap<Entity, Integer>> trackingEntries = new LinkedHashMap<>();
+    private static LinkedHashMap<EntityLivingBase, ArrayList<Pair<EntityLivingBase, Integer>>> trackingEntries = new LinkedHashMap<>();
 
-    public static void track(EntityLivingBase tracker, EntityLivingBase target, int ticks)
+    /**
+     * Negative duration is infinite duration
+     */
+    public static void track(EntityLivingBase tracker, EntityLivingBase target, int tickDuration)
     {
-        LinkedHashMap<Entity, Integer> map = trackingEntries.get(tracker);
-        if (map == null) map = new LinkedHashMap<>();
-        map.put(target, Tools.max(map.getOrDefault(target, ticks), ticks));
+        ArrayList<Pair<EntityLivingBase, Integer>> list = trackingEntries.computeIfAbsent(tracker, k -> new ArrayList<>());
+        list.add(new Pair<>(target, tickDuration));
     }
 
     public static boolean isTracking(EntityLivingBase tracker, Entity target)
     {
-        LinkedHashMap<Entity, Integer> map = trackingEntries.get(tracker);
-        return map != null && map.containsKey(target);
+        ArrayList<Pair<EntityLivingBase, Integer>> list = trackingEntries.get(tracker);
+        return list != null && list.stream().anyMatch(k -> k.getKey() == target);
     }
 
     @SubscribeEvent
     public static void update(TickEvent.ServerTickEvent event)
     {
-        trackingEntries.entrySet().removeIf(e -> updateMap(e.getValue()));
+        trackingEntries.entrySet().removeIf(e -> updateList(e.getValue()));
     }
 
-    private static boolean updateMap(LinkedHashMap<Entity, Integer> map)
+    private static boolean updateList(ArrayList<Pair<EntityLivingBase, Integer>> list)
     {
-        map.entrySet().removeIf(Tracking::updateEntry);
-        return map.size() <= 0;
+        list.removeIf(Tracking::updateEntry);
+        return list.size() == 0;
     }
 
-    private static boolean updateEntry(Map.Entry<Entity, Integer> entry)
+    private static boolean updateEntry(Pair<EntityLivingBase, Integer> pair)
     {
-        int val = entry.getValue();
-        if (--val <= 0) return true;
+        int val = pair.getValue();
+        if (val >= 0)
+        {
+            pair.setValue(val - 1);
+            return true;
+        }
 
-        entry.setValue(val);
         return false;
     }
 }
