a13d134021a5b800ad7cc9c3b0ba5ca32568d059
Kyle Koder <silvertallon@gmail.com>
2019-04-17 19:37:31 -0700

    Revert "1.12.2.068"
    This reverts commit f717fef067f7c3a553aa7f4046cc88ccfb31f4e6.

--- src/main/java/com/fantasticsource/dynamicstealth/DynamicStealth.java
+++ src/main/java/com/fantasticsource/dynamicstealth/DynamicStealth.java
@@ -93,7 +93,7 @@ public class DynamicStealth
 {
     public static final String MODID = "dynamicstealth";
     public static final String NAME = "Dynamic Stealth";
-    public static final String VERSION = "1.12.2.068";
+    public static final String VERSION = "1.12.2.067a";
     public static final String CONFIG_VERSION = "1.12.2.056"; //The lowest compatible config version
 
     public static final TrigLookupTable TRIG_TABLE = new TrigLookupTable(1024);
