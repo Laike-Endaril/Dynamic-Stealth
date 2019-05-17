a9b9dc0407b7ceaa6e65a0adddebff6d599dd121
Kyle Koder <silvertallon@gmail.com>
2019-05-16 22:15:06 -0700

    Increased sneak attack multipliers - dagger (3x -> 5x), kama (2x -> 3x)

--- src/main/java/com/fantasticsource/dynamicstealth/server/event/attacks/AttackDefaults.java
+++ src/main/java/com/fantasticsource/dynamicstealth/server/event/attacks/AttackDefaults.java
@@ -13,17 +13,19 @@ public class AttackDefaults
         normalAttackDefaults.add("dye, false, 0, , blindness.20, true");
         normalAttackDefaults.add("glowstone_dust, false, 0, , glowing.20, true");
 
-        stealthAttackDefaults.add("backstab:wood_dagger, true, 3");
-        stealthAttackDefaults.add("backstab:stone_dagger, true, 3");
-        stealthAttackDefaults.add("backstab:iron_dagger, true, 3");
-        stealthAttackDefaults.add("backstab:diamond_dagger, true, 3");
-        stealthAttackDefaults.add("backstab:gold_dagger, true, 3");
-        stealthAttackDefaults.add("natura:ghostwood_kama, true, 2");
-        stealthAttackDefaults.add("natura:bloodwood_kama, true, 2");
-        stealthAttackDefaults.add("natura:darkwood_kama, true, 2");
-        stealthAttackDefaults.add("natura:fusewood_kama, true, 2");
-        stealthAttackDefaults.add("natura:netherquartz_kama, true, 2");
-        stealthAttackDefaults.add("tetra:duplex_tool_modular > duplex/sickle_left_material & duplex/butt_right_material, true, 2");
-        stealthAttackDefaults.add("tconstruct:kama, true, 2");
+
+        stealthAttackDefaults.add("backstab:wood_dagger, true, 5");
+        stealthAttackDefaults.add("backstab:stone_dagger, true, 5");
+        stealthAttackDefaults.add("backstab:iron_dagger, true, 5");
+        stealthAttackDefaults.add("backstab:diamond_dagger, true, 5");
+        stealthAttackDefaults.add("backstab:gold_dagger, true, 5");
+
+        stealthAttackDefaults.add("natura:ghostwood_kama, true, 3");
+        stealthAttackDefaults.add("natura:bloodwood_kama, true, 3");
+        stealthAttackDefaults.add("natura:darkwood_kama, true, 3");
+        stealthAttackDefaults.add("natura:fusewood_kama, true, 3");
+        stealthAttackDefaults.add("natura:netherquartz_kama, true, 3");
+        stealthAttackDefaults.add("tetra:duplex_tool_modular > duplex/sickle_left_material & duplex/butt_right_material, true, 3");
+        stealthAttackDefaults.add("tconstruct:kama, true, 3");
     }
 }
