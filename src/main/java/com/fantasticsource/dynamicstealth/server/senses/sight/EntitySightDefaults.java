48c31c8f59bf6c334d5c673bdd5f33f9581c97bb
Kyle Koder <silvertallon@gmail.com>
2019-05-21 11:36:29 -0700

    Improved compat for Lycanite's Mobs

--- src/main/java/com/fantasticsource/dynamicstealth/server/senses/sight/EntitySightDefaults.java
+++ src/main/java/com/fantasticsource/dynamicstealth/server/senses/sight/EntitySightDefaults.java
@@ -14,6 +14,21 @@ public class EntitySightDefaults
         naturallyBrightDefaults.add("wither");
 
         //Compat; these should be added absolutely, not conditionally
-        //TODO add glowy lycanite stuff, like cinders, wisps, etc...
+        naturallyBrightDefaults.add("infernomobs:khalk");
+        naturallyBrightDefaults.add("infernomobs:afrit");
+        naturallyBrightDefaults.add("infernomobs:lobber");
+        naturallyBrightDefaults.add("infernomobs:gorger");
+        naturallyBrightDefaults.add("infernomobs:salamander");
+        naturallyBrightDefaults.add("infernomobs:cephignis");
+        naturallyBrightDefaults.add("infernomobs:ignibus");
+        naturallyBrightDefaults.add("demonmobs:rahovart");
+        naturallyBrightDefaults.add("elementalmobs:nymph");
+        naturallyBrightDefaults.add("elementalmobs:zephyr");
+        naturallyBrightDefaults.add("elementalmobs:wisp");
+        naturallyBrightDefaults.add("elementalmobs:cinder");
+        naturallyBrightDefaults.add("elementalmobs:banshee");
+        naturallyBrightDefaults.add("elementalmobs:aegis");
+        naturallyBrightDefaults.add("elementalmobs:sylph");
+        naturallyBrightDefaults.add("elementalmobs:xaphan");
     }
 }
