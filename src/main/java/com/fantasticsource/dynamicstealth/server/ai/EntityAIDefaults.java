6576fc1b9ccc2ecec2c6257ebea477afd465ec02
Kyle Koder <silvertallon@gmail.com>
2019-05-26 08:43:42 -0700

    undead horses and ebwizardry compat

--- src/main/java/com/fantasticsource/dynamicstealth/server/ai/EntityAIDefaults.java
+++ src/main/java/com/fantasticsource/dynamicstealth/server/ai/EntityAIDefaults.java
@@ -22,9 +22,21 @@ public class EntityAIDefaults
         fearlessDefaults.add("enderman");
         fearlessDefaults.add("ender_dragon");
         fearlessDefaults.add("wither");
+        fearlessDefaults.add("skeleton_horse");
+        fearlessDefaults.add("zombie_horse");
 
 
         //Compat; these should be added absolutely, not conditionally
         fearlessDefaults.add("harvestersnight:harvester");
+        fearlessDefaults.add("ebwizardry:skeleton_minion");
+        fearlessDefaults.add("ebwizardry:spirit_wolf");
+        fearlessDefaults.add("ebwizardry:ice_wraith");
+        fearlessDefaults.add("ebwizardry:lightning_wraith");
+        fearlessDefaults.add("ebwizardry:shadow_wraith");
+        fearlessDefaults.add("ebwizardry:magic_slime");
+        fearlessDefaults.add("ebwizardry:spirit_horse");
+        fearlessDefaults.add("ebwizardry:phoenix");
+        fearlessDefaults.add("ebwizardry:storm_elemental");
+        fearlessDefaults.add("ebwizardry:wither_skeleton_minion");
     }
 }
