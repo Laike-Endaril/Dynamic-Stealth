0b6531d72edbdfe84430fb094fc4bfe49bc01f3f
Kyle Koder <silvertallon@gmail.com>
2019-06-04 22:37:14 -0700

    Indev build 1.12.2.074b  Don't ask what happened to 1.12.2.074a

--- src/main/java/com/fantasticsource/dynamicstealth/DynamicStealth.java
+++ src/main/java/com/fantasticsource/dynamicstealth/DynamicStealth.java
@@ -93,7 +93,7 @@ public class DynamicStealth
 {
     public static final String MODID = "dynamicstealth";
     public static final String NAME = "Dynamic Stealth";
-    public static final String VERSION = "1.12.2.074";
+    public static final String VERSION = "1.12.2.074b";
     public static final String CONFIG_VERSION = "1.12.2.072"; //The lowest compatible config version
 
     public static final TrigLookupTable TRIG_TABLE = new TrigLookupTable(1024);
