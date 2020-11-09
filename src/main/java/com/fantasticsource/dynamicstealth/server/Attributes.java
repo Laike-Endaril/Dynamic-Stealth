9cfb32bce028e501f57940b8222c91e822196162
Kyle Koder <silvertallon@gmail.com>
2020-11-09 10:46:11 -0800

    Set all attributes to sync to client, so Tiamat Inventory and other mods can view them

--- src/main/java/com/fantasticsource/dynamicstealth/server/Attributes.java
+++ src/main/java/com/fantasticsource/dynamicstealth/server/Attributes.java
@@ -6,27 +6,27 @@ import net.minecraft.entity.ai.attributes.RangedAttribute;
 
 public class Attributes
 {
-    public static RangedAttribute THREATGEN = new RangedAttribute(null, DynamicStealth.MODID + ".threatGen", 1, 0, Double.MAX_VALUE);
-    public static RangedAttribute THREATGEN_SPOTTED = new RangedAttribute(THREATGEN, DynamicStealth.MODID + ".threatGenTargetSpotted", 1, 0, Double.MAX_VALUE);
-    public static RangedAttribute THREATGEN_ATTACK = new RangedAttribute(THREATGEN, DynamicStealth.MODID + ".threatGenAttackedBySame", 1, 0, Double.MAX_VALUE);
-    public static RangedAttribute THREATGEN_DAMAGE_TAKEN = new RangedAttribute(THREATGEN, DynamicStealth.MODID + ".threatGenDamageDealt", 1, 0, Double.MAX_VALUE);
-    public static RangedAttribute THREATGEN_WARNED_AGAINST = new RangedAttribute(THREATGEN, DynamicStealth.MODID + ".threatGenWarned", 1, 0, Double.MAX_VALUE);
-    public static RangedAttribute THREATGEN_KILL = new RangedAttribute(THREATGEN, DynamicStealth.MODID + ".threatGenAllyKilled", 1, 0, Double.MAX_VALUE);
-    public static RangedAttribute THREATGEN_VISIBLE = new RangedAttribute(THREATGEN, DynamicStealth.MODID + ".threatGenTargetVisible", 1, 0, Double.MAX_VALUE);
+    public static RangedAttribute THREATGEN = (RangedAttribute) new RangedAttribute(null, DynamicStealth.MODID + ".threatGen", 1, 0, Double.MAX_VALUE).setShouldWatch(true);
+    public static RangedAttribute THREATGEN_SPOTTED = (RangedAttribute) new RangedAttribute(THREATGEN, DynamicStealth.MODID + ".threatGenTargetSpotted", 1, 0, Double.MAX_VALUE).setShouldWatch(true);
+    public static RangedAttribute THREATGEN_ATTACK = (RangedAttribute) new RangedAttribute(THREATGEN, DynamicStealth.MODID + ".threatGenAttackedBySame", 1, 0, Double.MAX_VALUE).setShouldWatch(true);
+    public static RangedAttribute THREATGEN_DAMAGE_TAKEN = (RangedAttribute) new RangedAttribute(THREATGEN, DynamicStealth.MODID + ".threatGenDamageDealt", 1, 0, Double.MAX_VALUE).setShouldWatch(true);
+    public static RangedAttribute THREATGEN_WARNED_AGAINST = (RangedAttribute) new RangedAttribute(THREATGEN, DynamicStealth.MODID + ".threatGenWarned", 1, 0, Double.MAX_VALUE).setShouldWatch(true);
+    public static RangedAttribute THREATGEN_KILL = (RangedAttribute) new RangedAttribute(THREATGEN, DynamicStealth.MODID + ".threatGenAllyKilled", 1, 0, Double.MAX_VALUE).setShouldWatch(true);
+    public static RangedAttribute THREATGEN_VISIBLE = (RangedAttribute) new RangedAttribute(THREATGEN, DynamicStealth.MODID + ".threatGenTargetVisible", 1, 0, Double.MAX_VALUE).setShouldWatch(true);
 
-    public static RangedAttribute THREATDEG = new RangedAttribute(null, DynamicStealth.MODID + ".threatDeg", 1, 0, Double.MAX_VALUE);
-    public static RangedAttribute THREATDEG_NOT_VISIBLE = new RangedAttribute(THREATDEG, DynamicStealth.MODID + ".threatDegTargetNotVisible", 1, 0, Double.MAX_VALUE);
-    public static RangedAttribute THREATDEG_FLEE_FROM = new RangedAttribute(THREATDEG, DynamicStealth.MODID + ".threatDegFlee", 1, 0, Double.MAX_VALUE);
+    public static RangedAttribute THREATDEG = (RangedAttribute) new RangedAttribute(null, DynamicStealth.MODID + ".threatDeg", 1, 0, Double.MAX_VALUE).setShouldWatch(true);
+    public static RangedAttribute THREATDEG_NOT_VISIBLE = (RangedAttribute) new RangedAttribute(THREATDEG, DynamicStealth.MODID + ".threatDegTargetNotVisible", 1, 0, Double.MAX_VALUE).setShouldWatch(true);
+    public static RangedAttribute THREATDEG_FLEE_FROM = (RangedAttribute) new RangedAttribute(THREATDEG, DynamicStealth.MODID + ".threatDegFlee", 1, 0, Double.MAX_VALUE).setShouldWatch(true);
 
 
-    public static RangedAttribute VISIBILITY_REDUCTION = new RangedAttribute(null, DynamicStealth.MODID + ".visibilityReduction", 100, 0, Double.MAX_VALUE);
-    public static RangedAttribute SIGHT = new RangedAttribute(null, DynamicStealth.MODID + ".sight", 100, 0, Double.MAX_VALUE);
+    public static RangedAttribute VISIBILITY_REDUCTION = (RangedAttribute) new RangedAttribute(null, DynamicStealth.MODID + ".visibilityReduction", 100, 0, Double.MAX_VALUE).setShouldWatch(true);
+    public static RangedAttribute SIGHT = (RangedAttribute) new RangedAttribute(null, DynamicStealth.MODID + ".sight", 100, 0, Double.MAX_VALUE).setShouldWatch(true);
 
-    public static RangedAttribute NOISE_REDUCTION = new RangedAttribute(null, DynamicStealth.MODID + ".noiseReduction", 100, 0, Double.MAX_VALUE);
-    public static RangedAttribute HEARING = new RangedAttribute(null, DynamicStealth.MODID + ".hearing", 100, 0, Double.MAX_VALUE);
+    public static RangedAttribute NOISE_REDUCTION = (RangedAttribute) new RangedAttribute(null, DynamicStealth.MODID + ".noiseReduction", 100, 0, Double.MAX_VALUE).setShouldWatch(true);
+    public static RangedAttribute HEARING = (RangedAttribute) new RangedAttribute(null, DynamicStealth.MODID + ".hearing", 100, 0, Double.MAX_VALUE).setShouldWatch(true);
 
-    public static RangedAttribute SCENT_REDUCTION = new RangedAttribute(null, DynamicStealth.MODID + ".scentReduction", 100, 0, Double.MAX_VALUE);
-    public static RangedAttribute SMELLING = new RangedAttribute(null, DynamicStealth.MODID + ".smelling", 100, 0, Double.MAX_VALUE);
+    public static RangedAttribute SCENT_REDUCTION = (RangedAttribute) new RangedAttribute(null, DynamicStealth.MODID + ".scentReduction", 100, 0, Double.MAX_VALUE).setShouldWatch(true);
+    public static RangedAttribute SMELLING = (RangedAttribute) new RangedAttribute(null, DynamicStealth.MODID + ".smelling", 100, 0, Double.MAX_VALUE).setShouldWatch(true);
 
 
     public static void init()
