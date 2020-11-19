f0e8a3ea487e06a218aadbc2f843c237e7674c81
Kyle Koder <silvertallon@gmail.com>
2020-11-19 01:43:28 -0800

    1.12.2.111e

--- src/main/java/com/fantasticsource/dynamicstealth/DynamicStealth.java
+++ src/main/java/com/fantasticsource/dynamicstealth/DynamicStealth.java
@@ -99,7 +99,7 @@ public class DynamicStealth
 {
     public static final String MODID = "dynamicstealth";
     public static final String NAME = "Dynamic Stealth";
-    public static final String VERSION = "1.12.2.111d";
+    public static final String VERSION = "1.12.2.111e";
     public static final String CONFIG_VERSION = "1.12.2.095"; //The lowest compatible config version
 
     public static final TrigLookupTable TRIG_TABLE = new TrigLookupTable(1024);
