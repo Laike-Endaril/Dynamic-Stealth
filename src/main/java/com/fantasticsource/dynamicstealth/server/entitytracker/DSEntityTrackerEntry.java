e093d0fee7eb7294227a4381523f544df14a3621
Kyle Koder <silvertallon@gmail.com>
2024-05-12 18:46:37 -0700

    Revert "Think I fixed head rotation "glitch twitching" for seen entities that are riding unseen entities"
    This reverts commit ab465efeb67c7931ab53ac6598e06dd9e0e812a9.

--- src/main/java/com/fantasticsource/dynamicstealth/server/entitytracker/DSEntityTrackerEntry.java
+++ src/main/java/com/fantasticsource/dynamicstealth/server/entitytracker/DSEntityTrackerEntry.java
@@ -1,7 +1,6 @@
 package com.fantasticsource.dynamicstealth.server.entitytracker;
 
 import com.fantasticsource.dynamicstealth.server.senses.sight.Sight;
-import com.fantasticsource.tools.Tools;
 import net.minecraft.block.Block;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.AttributeMap;
@@ -86,26 +85,38 @@ public class DSEntityTrackerEntry extends EntityTrackerEntry
 
         if (updateCounter % updateFrequency == 0 || entity.isAirBorne || entity.getDataManager().isDirty())
         {
-            if (updateCounter > 0)
+            if (entity.isRiding())
             {
-                if (sendVelocityUpdates || (isLivingBase && livingBase.isElytraFlying()))
+                int newEncodedYaw = MathHelper.floor(entity.rotationYaw * 256 / 360);
+                int newEncodedPitch = MathHelper.floor(entity.rotationPitch * 256 / 360);
+                if (Math.abs(newEncodedYaw - encodedRotationYaw) >= 1 || Math.abs(newEncodedPitch - encodedRotationPitch) >= 1)
                 {
-                    if (lastMotionX != entity.motionX || lastMotionY != entity.motionY || lastMotionZ != entity.motionZ)
+                    sendPacketToTrackedPlayers(new SPacketEntity.S16PacketEntityLook(entity.getEntityId(), (byte) newEncodedYaw, (byte) newEncodedPitch, entity.onGround));
+                    encodedRotationYaw = newEncodedYaw;
+                    encodedRotationPitch = newEncodedPitch;
+                }
+            }
+            else
+            {
+                if (updateCounter > 0)
+                {
+                    if (sendVelocityUpdates || (isLivingBase && livingBase.isElytraFlying()))
                     {
-                        lastMotionX = entity.motionX;
-                        lastMotionY = entity.motionY;
-                        lastMotionZ = entity.motionZ;
-                        sendPacketToTrackedPlayers(new SPacketEntityVelocity(entity.getEntityId(), entity.motionX, entity.motionY, entity.motionZ));
+                        if (lastMotionX != entity.motionX || lastMotionY != entity.motionY || lastMotionZ != entity.motionZ)
+                        {
+                            lastMotionX = entity.motionX;
+                            lastMotionY = entity.motionY;
+                            lastMotionZ = entity.motionZ;
+                            sendPacketToTrackedPlayers(new SPacketEntityVelocity(entity.getEntityId(), entity.motionX, entity.motionY, entity.motionZ));
+                        }
                     }
+
+                    sendPacketToTrackedPlayers(new SPacketEntityTeleport(entity));
                 }
 
-                sendPacketToTrackedPlayers(new SPacketEntityTeleport(entity));
+                encodedRotationYaw = MathHelper.floor(entity.rotationYaw * 256 / 360);
+                encodedRotationPitch = MathHelper.floor(entity.rotationPitch * 256 / 360);
             }
-
-            entity.rotationYaw = Tools.posMod(entity.rotationYaw, 360);
-            entity.rotationPitch = Tools.posMod(entity.rotationPitch, 360);
-            encodedRotationYaw = MathHelper.floor(entity.rotationYaw * 256 / 360);
-            encodedRotationPitch = MathHelper.floor(entity.rotationPitch * 256 / 360);
 
             sendMetadata();
 
