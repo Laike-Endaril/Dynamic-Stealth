2d94584984d141accd0c1be6382cf27e0e96f1ff
Laike-Endaril <silvertallon@gmail.com>
2025-04-10 11:31:07 -0700

    1.12.2.119

--- src/main/java/com/fantasticsource/dynamicstealth/DynamicStealth.java
+++ src/main/java/com/fantasticsource/dynamicstealth/DynamicStealth.java
@@ -104,7 +104,7 @@ public class DynamicStealth
 {
     public static final String MODID = "dynamicstealth";
     public static final String NAME = "Dynamic Stealth";
-    public static final String VERSION = "1.12.2.118";
+    public static final String VERSION = "1.12.2.119";
     public static final String CONFIG_VERSION = "1.12.2.116"; //The lowest compatible config version
 
     public static final TrigLookupTable TRIG_TABLE = new TrigLookupTable(1024);
